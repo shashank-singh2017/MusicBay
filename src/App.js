6dd54c2f8012f207935e2972506dfbccb3f55afa
Shashank <shashank@Shashanks-MacBook-Pro.local>
2018-02-11 03:18:12 -0800

    changed something

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ sortPopularity()
 
 
           <div className="login">
-          <a href="https://authoriztioncode.herokuapp.com/">  <button className="login_Button"><span className="login_text">Log In to Spotify</span></button> </a>
+          <a href="https://glacial-ocean-96759.herokuapp.com/">  <button className="login_Button"><span className="login_text">Log In to Spotify</span></button> </a>
           </div>
 
 
