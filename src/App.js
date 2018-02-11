8f0410a1384e510868f0dc3f6eeac0ef0288b7c9
Shashank <shashank@Shashanks-MacBook-Pro.local>
2018-02-11 06:53:56 -0800

    changed callback

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ sortPopularity()
 
 
           <span className="col-sm-4" style={{float: 'right', marginRight: 20}}>
-          <a href="http://desolate-shelf-95963.herokuapp.com/"> <Button bsStyle="success">Login to Spotify</Button> </a>
+          <a href="http://desolate-shelf-95963.herokuapp.com/"> <ButtonToolbar><Button bsStyle="success">Login to Spotify</Button></ButtonToolbar>; </a>
           </span>
 
     </div>
