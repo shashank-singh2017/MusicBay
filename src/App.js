17e348cfcba3058f3220ddd2ba24287b1a442130
Shashank <shashank@Shashanks-MacBook-Pro.local>
2018-02-11 06:00:43 -0800

    changed callback

--- src/App.js
+++ src/App.js
@@ -105,6 +105,7 @@ sortPopularity()
   render() {
     return (
 <div>
+    <div class="jumbotron">
       <div className="App">
           <div className="logo">
           <p>MusicBay - Zikher &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  One Stop Destination for Music Tracks</p>
@@ -114,9 +115,8 @@ sortPopularity()
           <div className="login">
           <a href="http://desolate-shelf-95963.herokuapp.com/">  <button className="login_Button"><span className="login_text">Log In to Spotify</span></button> </a>
           </div>
-
-
       </div>
+    </div>
 <div className="mainBody">
 
         <div className='trackRow'>
