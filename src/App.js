0bcef5c0e16d19486db74a7ce8224ec2e5f382b4
Shashank <shashank@Shashanks-MacBook-Pro.local>
2018-02-11 21:45:35 -0800

    mofications to make mobile responsive rows

--- src/App.js
+++ src/App.js
@@ -145,19 +145,19 @@ sortPopularity()
                 <br/>
                 {this.state.tracks.map((track) => {
                   return (
-                  <div className="row" style={{marginTop: 20, backgroundColor: "#391285"}}>
+                  <div className="row" style={{marginTop: 20, backgroundColor: "#391285" color: "white"}}>
                     <div className="col-sm-10">
                         <div className="image col-sm-2">
                           <img src={track.album.images[2].url} />
                         </div>
                         <div className="col-sm-10">
-                          <div className="col-sm-5" style={{paddingLeft: 20, marginLeft: "3%", paddingTop: 10}}>
+                          <div className="col-sm-5" style={{paddingLeft: 20, marginLeft: "3%", paddingTop: 10, color: "white"}}>
                             <span>Title: {track.name}</span><br/>
                             <span> Type: {track.album.album_type}</span><br/>
                             <span> Duration: {Math.floor(track.duration_ms/60000)}:{Math.floor((track.duration_ms/1000)%60)} </span>
                           </div>
 
-                          <div className="col-sm-5" style={{paddingLeft: 20, marginTop: 10}}>
+                          <div className="col-sm-5" style={{paddingLeft: 20, marginTop: 10, color: "white"}}>
                             <span>Popularity: {track.popularity}</span><br/>
                             <span> Artists: </span><br/>
                             <a href={track.external_urls.spotify}><button style={{height: 25}}>Open in Spotify</button></a>
